6d9c32cd7c280cb9f635f85b7a223f83c27e3378
Steffano <smalagadev@gmail.com>
2019-12-28 17:46:44 -0500

    Sample records inserted in to all table except Reimbursements

--- script.sql
+++ script.sql
@@ -1,17 +1,31 @@
 CREATE TABLE Status(
   status_id SERIAL PRIMARY KEY,
-  status VARCHAR(10)
+  status VARCHAR(25)
 );
+
+INSERT INTO Status(status_id, status) VALUES(0, 'Pending');
+INSERT INTO Status(status_id, status) VALUES(1, 'Approved');
+INSERT INTO Status(status_id, status) VALUES(2, 'Denied');
 
 CREATE TABLE Reimbursement_Type(
   type_id SERIAL PRIMARY KEY,
-  type VARCHAR(10)
+  type VARCHAR(25)
 );
+
+INSERT INTO Reimbursement_Type(type_id, type) VALUES(0, 'Lodging');
+INSERT INTO Reimbursement_Type(type_id, type) VALUES(1, 'Travel');
+INSERT INTO Reimbursement_Type(type_id, type) VALUES(2, 'Food');
+INSERT INTO Reimbursement_Type(type_id, type) VALUES(3, 'Other');
 
 CREATE TABLE User_Roles(
   role_id SERIAL PRIMARY KEY,
-  role VARCHAR(10)
+  role VARCHAR(25)
 );
+
+INSERT INTO User_Roles(role_id, role) VALUES(0, 'Finance Manager');
+INSERT INTO User_Roles(role_id, role) VALUES(1, 'Employee');
+INSERT INTO User_Roles(role_id, role) VALUES(2, 'Recruiter');
+INSERT INTO User_Roles(role_id, role) VALUES(3, 'Accountant');
 
 CREATE TABLE Users(
 	users_id SERIAL PRIMARY KEY,
@@ -23,6 +37,19 @@ CREATE TABLE Users(
   role_id INT,
 	FOREIGN KEY(role_id) REFERENCES User_Roles(role_id)
 );
+
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('jberrymore', 368540387, 'Jerry', 'Berrymore', 'jerry.berrymore@ers.com', 1);
+-- password - berrytime
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('mbolton', 197113697, 'Michael', 'Bolton', 'michael.bolton@ers.com', 1);
+-- password - notthatmichaelbolton
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('amahoney', 1216985755, 'Alton', 'Mahoney', 'alton.mahoney@ers.com', 1);
+-- password - password
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('chill', 1659762602, 'Carmen', 'Hill', 'carmen.hill@ers.com', 0);
+-- password - chillout
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('tcrewes', 1438651681, 'Thomas', 'Crewes', 'thomas.crewes@ers.com', 2);
+-- password - missionimpossible
+INSERT INTO USERS (username, password, first_name, last_name, email, role_id) VALUES('achocula', -1022649593, 'Aaron', 'Chocula', 'aaron.chocula@ers.com', 3);
+-- password - countmeout
 
 CREATE TABLE Reimbursement (
 	reimbursement_id SERIAL PRIMARY KEY,
