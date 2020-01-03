7097fc5e72fa51024459e9e534317b3eed386e2c
Steffano <smalagadev@gmail.com>
2020-01-03 15:28:31 -0500

    Login && Logout functionality added to navigation bar

--- ers/src/app/components/navbar/navbar.component.ts
+++ ers/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @Input() public isLoggedIn: boolean;
+
+  // Opens Login form in Modal
+  loginForm(): void{
+    const loginForm = document.getElementById('Login');
+    // Returns error because TypeScript does not recognize Materialize CDN in HTML file
+    const instances = M.Modal.init(loginForm);
+  }
+
+  logoutForm(): void{
+    const logoutForm = document.getElementById('Logout');
+    // Returns error because TypeScript does not recognize Materialize CDN in HTML file
+    const instances = M.Modal.init(logoutForm);
   }
 
 }
