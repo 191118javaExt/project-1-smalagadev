f6a15bab4220d387c52dd9d43877f9490b3b6ac4
Steffano <smalagadev@gmail.com>
2020-01-06 15:27:47 -0500

    Manage Reimbursements route added

--- ers/src/app/app-routing.module.ts
+++ ers/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { NewReimbursementComponent } from './components/new-reimbursement/new-reimbursement.component';
+import { HistoryComponent } from './components/history/history.component';
+import { ManageReimbursementsComponent } from './components/manage-reimbursements/manage-reimbursements.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'new_reimbursement',
+    component: NewReimbursementComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  },
+  {
+    path: 'manage_reimbursements',
+    component: ManageReimbursementsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
