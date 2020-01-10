154c29f8d0fdf107dce4d6a87d9928fa68a56e88
Steffano <smalagadev@gmail.com>
2020-01-10 00:33:49 -0500

    User replaces Employee

---
+++ ers/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+  constructor(
+  //   private id: int,
+  // 	private amount: double,
+  // 	private submitted: LocalDate,
+  // 	private resolved: LocalDate,
+  // 	private description: String,
+  // //	private receipt
+  // 	private author: int,
+  // 	private resolver: int,
+  // 	private status_id: int,
+  // 	private type_id: int
+  ){}
+}
