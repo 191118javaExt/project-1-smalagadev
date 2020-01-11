0681d7cf4b38dc2ea82556676280e2347ebfc53e
Steffano <smalagadev@gmail.com>
2020-01-10 19:28:16 -0500

    Services instantiated

--- ers/src/app/services/reimbursement.service.ts
+++ ers/src/app/services/reimbursement.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reimbursement } from '../models/reimbursement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReimbursementService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  insert(r:Reimbursement ):Observable<Reimbursement>{
+    let body:any = {
+
+    }
+
+    return this.http.post<Reimbursement>('http://localhost:8080/reimbursement', body);
+  }
+
+  viewAll():Observable<Reimbursement>{
+    let body:any = {}
+
+    return this.http.post<Reimbursement>('http://localhost:8080/reimbursement', body);
+  }
 }
